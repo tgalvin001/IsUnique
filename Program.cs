3a578d6b7c6b270413e1fda31cbe92156af418c8
tgalvin001 <tgalvin001@gmail.com>
2019-06-29 22:50:51 -0500

    Determine if a string has all unique characters

--- Program.cs
+++ Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace IsUnique
 {
     class Program
     {
+        public static int IsUnique(List<char> c)
+        {
+            int flag = 0;
+            if (c.Count == 1)
+                return flag;
+            while (c.Count >= 2)
+            {
+                if (flag == 1)
+                    return flag;
+                char bruteForce = c[0];
+                c.RemoveAt(0);
+
+                if (c.Contains(bruteForce))
+                {
+                    Console.WriteLine("Got em");
+                    flag++;
+                }
+                Console.WriteLine(flag);
+
+            }
+            return flag;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            List<char> userInput = new List<char>();
+            userInput.AddRange(Console.ReadLine());
+            int flags = IsUnique(userInput);
+            
+            if (flags == 0)
+                Console.WriteLine("The string contains unique characters");
+            else
+                Console.WriteLine("The string does not contain unique characters");
+            
+                
+            
+            Console.ReadKey();
         }
     }
 }
